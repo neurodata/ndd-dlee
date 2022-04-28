7f84818f4776808f9e537565cf6e1f47c8a6f35f
Diane Lee <d0156lee@gmail.com>
2022-04-28 02:18:15 -0400

    adding cell tags

--- docs/add_cell_tags.py
+++ docs/add_cell_tags.py
@@ -1,27 +1,21 @@
 import nbformat as nbf
 from glob import glob
+import numpy as np
 
 # Collect a list of all notebooks in the content folder
 notebooks = glob("ndd-dlee/docs/**/*.ipynb", recursive=True)
 
-# Text to look for in adding tags
-text_search_dict = {
-    "# HIDDEN": "remove-cell",  # Remove the whole cell
-    "# NO CODE": "remove-input",  # Remove only the input
-    "# HIDE CODE": "hide-input"  # Hide the input w/ a button to show
-}
-
-# Search through each notebook and look for the text, add a tag if necessary
+# Search through each notebook and add hide-input tag
 for ipath in notebooks:
     ntbk = nbf.read(ipath, nbf.NO_CONVERT)
 
     for cell in ntbk.cells:
-        cell_tags = cell.get('metadata', {}).get('tags', [])
-        for key, val in text_search_dict.items():
-            if key in cell['source']:
-                if val not in cell_tags:
-                    cell_tags.append(val)
-        if len(cell_tags) > 0:
-            cell['metadata']['tags'] = cell_tags
+        if cell.get("cell_type") == "code":
+            cell_tags = cell.get("metadata", {}).get("tags", [])
+            cell_tags = list(np.unique(cell_tags))
+            if "hide-input" not in cell_tags:
+                cell_tags.append("hide-input")
 
     nbf.write(ntbk, ipath)
+
+print('Add cell tag')
